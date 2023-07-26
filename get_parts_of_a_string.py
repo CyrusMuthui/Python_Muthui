9532b26fc7f0e2326b59ea4d656ac29191a3664d
CyrusMuthui <cyrus.muthui20@gmail.com>
2023-07-26 06:11:43 +0300

    Scripting with python

---
+++ get_parts_of_a_string.py
@@ -0,0 +1,9 @@
+#!/bin/bash/python3
+word = "Cyrus Muthui is the best"
+word_first_12 = word[:12]
+word_last_6 = word[6:]
+middle_word = word[1:-1]
+
+print(f"The names is: {word_first_12}")
+print(f"The quality is: {word_last_6}")
+print(f"Playing with things: {middle_word}")
